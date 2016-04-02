8f3182b7786c1a4693b1e874529c7717a0f6da50
Louis <LOHankins@gmail.com>
2016-04-02 17:28:37 -0400

    First commit for jquery pixart

--- pixart.js
+++ pixart.js
@@ -1,1 +1,24 @@
+$(document).on("ready", function() {
+	event.preventDefault();
 
+
+var setColorButton = $("#set-color");
+ 
+
+function handleClickButton() {
+	console.log("handleClickButton");
+	var chosenColor = $("#color-field").val();
+	var brushBox = $(".brush");
+	$(".brush").css("background-color", chosenColor);
+	event.preventDefault();
+	$("#color-field").val("");
+}
+
+
+
+
+
+
+
+$("#set-color").click(handleClickButton);
+});
